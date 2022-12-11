a0d6637d5aecf1b08747451508c937c74347d56f
Mufti Alie Satriawan <iyakiwan19@gmail.com>
2022-12-11 18:13:39 +0700

    Initial Commit

---
+++ cheapy/cheapyApp.swift
@@ -0,0 +1,17 @@
+//
+//  cheapyApp.swift
+//  cheapy
+//
+//  Created by Mufti Alie Satriawan on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+struct cheapyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
